c7fba98c4e874944aaf10e22a0a4df63138c9184
haifu_qin <haifu_qin@163.com>
2023-05-28 20:55:17 +0800

    refs #0 leetcode No810 answer

---
+++ NO810.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    bool xorGame(vector<int>& nums) {
+        int size = nums.size();
+        if (size % 2 == 0) {
+            return true;
+        }
+        int xorsum = 0;
+        for (int i = 0; i < size; ++i) {
+            xorsum ^= nums[i];
+        }
+        return xorsum == 0;
+    }
+};
+
+int main()
+{
+    Solution s;
+
+
+    std::cout << "Hello World!\n";
+}
